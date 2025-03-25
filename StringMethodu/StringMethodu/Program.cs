573957fda994c480cd7bb548c32df14e187a4d3f
LamanZaman <zamanovaleman11@gmail.com>
2025-03-26 02:18:06 +0400

    third commit

---
+++ StringMethodu/StringMethodu/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StringMethodu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "        Menə lazIm olan hər şeY bir   fiNcan qəhvədir     ";
+            string formattedText = RightSentences.FormatText(input);
+            Console.WriteLine(formattedText);
+
+
+        }
+    }
+}
---
+++ StringMethodu/StringMethodu/RightSentences.cs
@@ -0,0 +1,17 @@
+﻿namespace StringMethodu
+{
+    internal class RightSentences
+    {
+        public static string FormatText(string text)
+        {
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return string.Join(" ", words.Select(CapitalizeWord));
+        }
+
+        private static string CapitalizeWord(string word)
+        {
+            if (string.IsNullOrEmpty(word)) return word;
+            return char.ToUpper(word[0]) + word.Substring(1).ToLower();
+        }
+    }
+}
