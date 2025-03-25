e1b9083b9cd6dce4f174e2211ea6fae7ff7bdfdb
LamanZaman <zamanovaleman11@gmail.com>
2025-03-26 01:42:26 +0400

    first commit

---
+++ class_inheritance/class_inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace class_inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student1 = new Student("Leman", "Zamanova", "BB209", 85, true);
+            Student student2 = new Student("Elsad", "Rzazade", "BB208", 93, true);
+            student1.GetInfo();
+            student1.GetDiplomDegree();
+            student1.CheckGraduation();
+            student2.GetInfo();
+            student2.CheckGraduation();
+            student2.GetDiplomDegree();
+        }
+    }
+}
---
+++ class_inheritance/class_inheritance/Student.cs
@@ -0,0 +1,56 @@
+﻿namespace class_inheritance
+{
+    internal class Student
+    {
+        public string name;
+        public string surname;
+        public string group;
+        public int point;
+        public bool IsGraduated;
+
+        public Student(string name, string surname, string group, int point, bool IsGraduated)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.group = group;
+            this.point = point;
+            this.IsGraduated = IsGraduated;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {name} {surname} Group:{group} point:{point} Graduate status:{IsGraduated}");
+        }
+
+        public void CheckGraduation()
+        {
+            if (IsGraduated)
+            {
+                Console.WriteLine("graduated");
+            }
+            else
+            {
+                Console.WriteLine("didn't graduate");
+            }
+        }
+        public void GetDiplomDegree()
+        {
+            if (point > 90)
+            {
+                Console.WriteLine("Distinction diploma");
+            }
+            else if (point > 80 && point <= 90)
+            {
+                Console.WriteLine("Merit diploma");
+            }
+            else if (point >= 65 && point <= 80)
+            {
+                Console.WriteLine("Past diploma");
+            }
+            else
+            {
+                Console.WriteLine("will not receive a diploma");
+            }
+        }
+
+    }
+}
